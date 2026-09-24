4c906dbed9550fe3a4ee6da59d8f2a59c18843c0
samoli1130 <seo5@email.sc.edu>
2026-09-24 13:43:32 -0400

    initial push for lab 5

---
+++ lab5/script.js
@@ -0,0 +1,60 @@
+let currentSavings = 10000;
+const withdrawClick = 100;
+const depositClick
+
+function withdrawAction() {
+    currentSavings = currentSavings - withdrawClick;
+
+    const healthText = document.getElementById("savings-display");
+    const statusText = document.getElementById("status-message");
+
+    healthText.innerText = currentSavings;
+
+    if (currentSavings > 0)
+         {
+        savingsText.innerText = currentSavings;
+        statusText.innerText = "Withdrew $100!";
+
+    }
+    else 
+    {
+        savingsText.innerText = 0;
+        statusText.innerText = "Out of Money!";
+
+        statusText.style.color = "#f1402c";
+        statusText.style.fontWeight = "bold";
+        document.body.style.backgroundColor = "#5a1a1a";
+
+        document.querySelector("button").disabled = true;
+        document.querySelector("button").innerText = "Dead";
+    }
+}
+
+function depositAction () {
+    currentSavings = currentSavings + depositClick;
+
+    const savingsText = document.getElementById("savings-display");
+    const statusText = document.getElementById("status-message");
+
+    healthText.innerText = currentSavings;
+
+    if (currentSavings > 0)
+         {
+        savingsText.innerText =  currentSavings;
+        statusText.innerText = "Deposited $100!";
+
+    }
+    else 
+    {
+        savingsText.innerText = 100000000;
+        statusText.innerText = "You Win!";
+
+        statusText.style.color = "#067415";
+        statusText.style.fontWeight = "bold";
+        document.body.style.backgroundColor = "#1a5a23";
+
+        document.querySelector("button").disabled = true;
+        document.querySelector("button").innerText = "Dead";
+
+}
+}
